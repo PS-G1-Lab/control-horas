267432d1989330880dd826f7c4e4c9f8e07d3ab8
XxMARRExX <xx_marre_xx@hotmail.com>
2024-03-08 12:25:13 +0000

    Visión de los componentes

---
+++ src/components/boxTeacherRol.svelte
@@ -0,0 +1,9 @@
+<div class="boxTeacherRol"></div>
+
+<style>
+  .boxTeacherRol {
+    width: 100px; /* Ancho del cuadrado */
+    height: 100px; /* Alto del cuadrado */
+    background-color: yellow; /* Color de fondo del cuadrado */
+  }
+</style>
