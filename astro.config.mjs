e3f7fc7e8e69c9595ee496efacce828d96be32c8
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2024-03-16 09:21:00 +0000

    Update Astro config imports and add integration options

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
 
-import svelte from '@astrojs/svelte'
+import svelte from "@astrojs/svelte"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+	integrations: [tailwind(), svelte()],
+	// output: "hybrid",
 })
