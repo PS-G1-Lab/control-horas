5bf2c7ff107feb00d5a4cb2c2b7e4327714751ed
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2024-03-18 03:17:25 +0000

    Add login API endpoint

---
+++ src/pages/api/login.ts
@@ -0,0 +1,23 @@
+import type { APIRoute } from "astro"
+
+export const POST: APIRoute = async ({ request }) => {
+	const formData = await request.formData()
+	const userData = {}
+	for (const [key, value] of formData.entries()) {
+		userData[key] = value
+	}
+
+	// const response = await fetch("https://control-horas-api-dev-zjjb.2.ie-1.fl0.io/user/login", {
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+	const response = await fetch("http://localhost:1234/user/login", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(userData),
+	})
+		.then((response) => response.json())
+		.catch((error) => error as Error)
+
+	return new Response(JSON.stringify(response))
+}
