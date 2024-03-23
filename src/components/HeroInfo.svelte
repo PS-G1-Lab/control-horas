e31d737c5f7bcf39742003f48da5190bf0d336bb
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2024-03-23 23:57:54 +0000

    Refactor HeroInfo component

--- src/components/HeroInfo.svelte
+++ src/components/HeroInfo.svelte
@@ -1,7 +1,5 @@
 <script>
 	import RoleButtons from "@/components/RoleButtons.svelte"
-	import QuestionMark from "@/icons/QuestionMark.svelte"
-	import RightArrow from "@/icons/RightArrow.svelte"
 	import { ROLE_STATUS, appStatus } from "@/store.ts"
 </script>
 
@@ -12,7 +10,7 @@
 		{#if $appStatus === ROLE_STATUS.STUDENT}
 			<p class="mb-5">Encuentra y contacta gratis con profesores particulares.</p>
 			<p>Busca a tu profesor según tus necesidades, experiencia y presupuesto.</p>
-		{:else if $appStatus === ROLE_STATUS.TEACHER}
+		{:else}
 			<p class="mb-5">Transmite tus conocimientos y gana dinero impartiendo clases particulares.</p>
 			<p>Tú pones las reglas; establece tus horarios y tus precios.</p>
 		{/if}
@@ -21,10 +19,18 @@
 		<a
 			href="/signup"
 			class="text-white bg-primary py-2 px-4 rounded-md flex items-center justify-center gap-3"
-			>Comenzar <RightArrow /></a
+			>Comenzar <img
+				src="/public/icons/rightArrow.svg"
+				alt="Flecha apuntando hacia la derecha"
+				class="w-4 h-auto"
+			/></a
 		>
 		<a href="/faqs" aria-label="Enlace que te lleva a la página de preguntas frecuentes"
-			><QuestionMark /></a
+			><img
+				src="/public/icons/questionMark.svg"
+				alt="Cierre de interrogación"
+				class="w-8 h-auto"
+			/></a
 		>
 	</div>
 </div>
