92131792cb9962e6dee46cb1ae40fa7586a2626d
Kilamper <kilamper@gmail.com>
2024-05-17 00:38:19 +0100

    chore: Create class details page for teacher and implement delete class function

---
+++ src/pages/api/deleteclass.ts
@@ -0,0 +1,23 @@
+import type { APIRoute } from "astro"
+
+export const POST: APIRoute = async ({ request }) => {
+	const formData = await request.formData()
+	const classData = {}
+	for (const [key, value] of formData.entries()) {
+		classData[key] = value
+	}
+
+	// const response = await fetch("http://localhost:1234/class/delete", {
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+	const response = await fetch("https://control-horas-api.onrender.com/class/delete", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(classData),
+	})
+		.then((response) => response.json())
+		.catch((error) => error as Error)
+
+	return new Response(JSON.stringify(response))
+}
